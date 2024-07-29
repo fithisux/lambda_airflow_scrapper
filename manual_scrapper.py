6f581fc3e926f9878041dae25c44d350f1f33e22
Vasileios Anagnostopoulos <vasilis.anagnostopoulos@agileactors.com>
2024-07-29 11:49:26 +0300

    feat: arg parsing and ruff

--- manual_scrapper.py
+++ manual_scrapper.py
@@ -1,4 +1,15 @@
 from scrapping import scrapper
+import argparse
 
 if __name__ == "__main__":
-    scrapper.do_scrapping(use_mongo=False)
+    parser = argparse.ArgumentParser(description="Scrap news.")
+    parser.add_argument(
+        "-m",
+        "--mongohost",
+        action="store",
+        dest="mongohost",
+        default=None,
+        help="Shall we use some mongo host or just crawl to scrapped_data_folder (file system)?",
+    )
+    args = parser.parse_args()
+    scrapper.do_scrapping(mongohost=args.mongohost)
